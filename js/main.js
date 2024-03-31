cf6a79a7ea57c3fd64e9a8e545545d6b90c36596
kandersson135 <kandersson135@gmail.com>
2024-03-31 13:12:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
     var $title = $listItem.find('.list-item-title');
     var statusOptions = ['is-danger', 'is-success'];
     var currentStatus = $statusButton.hasClass('is-danger') ? 'is-danger' : 'is-success';
-    //var currentAudio = $statusButton.hasClass('is-danger') ? onAudio.play() : offAudio.play();
+    var currentAudio = $statusButton.hasClass('is-danger') ? onAudio.play() : offAudio.play();
     var currentIcon = $statusButton.hasClass('is-danger') ? $(this).html('<i class="fas fa-check"></i>') : $(this).html('<i class="fas fa-minus"></i>');
     var currentIndex = statusOptions.indexOf(currentStatus);
     var newIndex = (currentIndex + 1) % statusOptions.length;
